ff85967dae2626fe04d8d1667d71b7117afc2e8a
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-02-25 21:44:02 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.32%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+  
+    int compareVersion(string s1, string s2) 
+    {
+        int n1 = s1.length(); 
+        int n2 = s2.length(); 
+        
+        int i = 0, j = 0; 
+        int number1 = 0, number2 = 0;  
+        while(i < n1 || j < n2)
+        {
+            // generating number between dots for string s1
+            while(i < n1 && s1[i] != '.')
+            {
+                number1 = number1 * 10 + (s1[i] - '0');
+                i++;
+            }
+            
+            // generating number between dots for string s2
+            while(j < n2 && s2[j] != '.')
+            {
+                number2 = number2 * 10 + (s2[j] - '0');
+                j++;
+            }
+            
+            // if number1 is greater than number2, from here return 1
+            if(number1 > number2)
+            {
+                return 1;
+            }
+            
+             // if number1 is less than number2, from here return -1
+            if(number1 < number2)
+            {
+                return -1;
+            }
+            
+            // if equal then we have to proceed further
+            // again give numbers to zero, as we again generate numbers b/w dots 
+            number1 = 0, number2 = 0;
+            
+            // move both pointers
+            i++;
+            j++;
+        }
+    
+        return 0;
+        
+    }
+};

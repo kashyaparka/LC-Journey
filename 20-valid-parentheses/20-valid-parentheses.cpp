a2cf34f71e7886c85406cbfe6154232cd3dd003c
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-03-13 19:16:45 +0530

    Time: 5 ms (18.36%), Space: 6.2 MB (80.05%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        if(s.size()<2) return false;
+        stack<char> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else if(s[i]==')')
+            {
+                if(st.empty() || st.top()!='(')
+                    return false;
+                else st.pop();
+            }
+            else if(s[i]=='}')
+            {
+                 if(st.empty() || st.top()!='{')
+                    return false;
+                else 
+                    st.pop();
+            }
+            else if(s[i]==']')
+            {
+                if(st.empty() || st.top()!='[')
+                    return false;
+                else st.pop();
+            }
+        }
+        return st.empty()?true:false;
+    }
+};

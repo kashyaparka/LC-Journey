cb72bb8dab3303aef5b0b97ac6ca440f0aeb268b
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-05-21 19:26:16 +0530

    Time: 11 ms (64.99%), Space: 13 MB (19.59%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -3,13 +3,9 @@ public:
     vector<int> getConcatenation(vector<int>& nums) 
     {
         vector<int> ans;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<2*nums.size();i++)
         {
-            ans.push_back(nums[i]);
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            ans.push_back(nums[i]);
+            ans.push_back(nums[i%nums.size()]);
         }
         return ans;
     }

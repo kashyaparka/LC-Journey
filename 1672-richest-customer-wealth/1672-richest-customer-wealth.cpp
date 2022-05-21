0956f546a2bf3ff2f6978bd5966c9b53cdc31c5b
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-05-21 19:37:58 +0530

    Time: 9 ms (34.66%), Space: 7.9 MB (52.62%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) 
     {
+        int n=accounts.size();
+        int m=accounts[0].size();
         int mx=INT_MIN;
-        for(int i=0;i<accounts.size();i++)
+        for(int i=0;i<n;i++)
         {
             int sum=0;
-            for(int j=0;j<accounts[0].size();j++)
+            for(int j=0;j<m;j++)
             {
                 sum+=accounts[i][j];
             }
-            mx=max(sum,mx);
+              mx=max(sum,mx);
         }
         return mx;
     }

7d724c94abcb32c7d48d1d94cfb9ae64a32d7955
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-05-24 12:48:24 +0530

    Added solution - LeetHub

---
+++ Palindromic Array - GFG/palindromic-array.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*Complete the function below*/
+#include<bits/stdc++.h>
+class Solution {
+public:
+ 
+    int PalinArray(int a[], int n)
+    {
+         for(int i=0;i<n;i++)
+         {
+             string s=to_string(a[i]);
+             string m=s;
+             reverse(s.begin(),s.end());
+             if(s!=m) return 0; 
+         }
+         return 1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i = 0; i < n; i++)
+			cin>>a[i];
+		Solution obj;
+		cout<<obj.PalinArray(a,n)<<endl;
+	}
+}  // } Driver Code Ends

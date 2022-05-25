d48d50ba8a058f0b82b80046b7e53303200419a1
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-05-25 18:07:50 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -8,23 +8,48 @@ class Solution{
 public:
 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) 
 	{
-      int mx=0;
-      int index=-1;
-      int k=0;
-      for(int i=0;i<n;i++)
-      {
-          int c=0;
-          for(int j=0;j<m;j++)
-          {
-              if(arr[i][j]==1)
-                c++;
+    //   int mx=0;
+    //   int index=-1;
+    //   int k=0;
+    //   for(int i=0;i<n;i++)
+    //   {
+    //       int c=0;
+    //       for(int j=0;j<m;j++)
+    //       {
+    //           if(arr[i][j]==1)
+    //             c++;
                   
-          }
-          if(mx<c)  index=k;
-          mx=max(mx,c);
-          k++;
-      }
-      return index;
+    //       }
+    //       if(mx<c)  index=k;
+    //       mx=max(mx,c);
+    //       k++;
+    //   }
+    //   return index;
+      
+   int count = 0,ans=-1;    // Using Binary Search O(mLog(n))
+   int i=0;
+   for(;i<n;i++)
+   {
+       int l=0,h=m-1,mid;
+       while(l<=h)
+       {
+           mid = l + (h-l)/2;
+           
+           if(arr[i][mid] < 1)
+           l = mid+1;
+           else
+           h = mid-1;
+ 
+       }
+       
+       if( count < m-l )
+       {
+            count = m-l;
+            ans = i;
+       }
+       
+   }
+   return ans;
 	}
 
 };

5cbb1659eec7eda25920dd0406d2e3f36de5bfb1
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-06-13 19:48:28 +0530

    Added solution - LeetHub

---
+++ Remove duplicates from an unsorted linked list - GFG/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+ // } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates(Node *head) 
+    {
+           unordered_set<int> s;
+           Node* curr=head;
+           Node* n=head->next;
+           s.insert(curr->data);
+           while(n!=NULL)
+           {
+               if(s.find(n->data)!=s.end())
+               {
+                   curr->next=n->next;
+                   n=n->next;
+               }
+               else
+               {
+                   s.insert(n->data);
+                   curr=curr->next;
+                   n=n->next;
+               }
+           }
+           return head;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}  // } Driver Code Ends

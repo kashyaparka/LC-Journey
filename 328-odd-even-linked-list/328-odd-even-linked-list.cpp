a211ce4df81fd1fe806ce5cf2c8c8d73f9ac0922
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-08-09 19:40:20 +0530

    Time: 12 ms (87.42%), Space: 10.5 MB (75.18%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -12,33 +12,55 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) 
     {
-        vector<int> odd,even;
-        ListNode* p=head;
-        int i=1;
-        while(p)
+        if(!head || !head->next) return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenhead=even;
+        while(even && even->next)
         {
-            if(i%2==1)
-                odd.push_back(p->val);
-            else if(i%2==0)
-                even.push_back(p->val);
-            i++;
-            p=p->next;
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
         }
-        p=head;
-        i=0;
-        while(p && i<odd.size())
-        {
-            p->val=odd[i];
-            p=p->next;
-            i++;
-        }
-        i=0;
-        while(p && i<even.size())
-        {
-            p->val=even[i];
-            p=p->next;
-            i++;
-        }
+        odd->next=evenhead;
         return head;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // vector<int> odd,even;
+        // ListNode* p=head;
+        // int i=1;
+        // while(p)
+        // {
+        //     if(i%2==1)
+        //         odd.push_back(p->val);
+        //     else if(i%2==0)
+        //         even.push_back(p->val);
+        //     i++;
+        //     p=p->next;
+        // }
+        // p=head;
+        // i=0;
+        // while(p && i<odd.size())
+        // {
+        //     p->val=odd[i];
+        //     p=p->next;
+        //     i++;
+        // }
+        // i=0;
+        // while(p && i<even.size())
+        // {
+        //     p->val=even[i];
+        //     p=p->next;
+        //     i++;
+        // }
+        // return head;
     }
 };

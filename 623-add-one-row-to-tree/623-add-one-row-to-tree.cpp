d84882109040dc867e45c6b6df0217ed48e568e9
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-10-05 18:58:49 +0530

    Time: 39 ms (34.83%), Space: 25.2 MB (16.59%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -11,61 +11,27 @@
  */
 class Solution {
 public:
-//     void solve(TreeNode* root,int val,int depth)
-//     {
-//         if(!root) return;
-      
-     
-//             TreeNode* k= new TreeNode(val);
-//             TreeNode* k1=root->left;
-//             k->left=k1;
-        
-//         if(root->right)
-//         {
-//               cout<<"kela";
-//             TreeNode* k= new TreeNode(val);
-//             TreeNode* k1=root->right;
-//             k->right=k1;
-//         }
-//     }
-//     void targetDepth(TreeNode* root,int val,int depth,int k)
-//     {
-//         if(!root) return;
-        
-//         if(k==depth) 
-//         {
-//             cout<<k<<" ";
-//             solve(root,val,depth);
-//         }
-//         targetDepth(root->left,val,depth,k+1);
-//         targetDepth(root->right,val,depth,k+1);
-//     }
-    TreeNode* addOneRow(TreeNode* root, int val, int d) 
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) 
     {
-//         if(!root) return nullptr;
-        
-//         targetDepth(root->left,val,depth,1);
-//         return root;
-        if(d==1) return new TreeNode(val,root,NULL);
-    
+        if(depth==1) return new TreeNode(val,root,NULL);
         queue<TreeNode*> q;
         q.push(root);
-    
-        while(d--)
+        
+        while(depth--)
         {
             int n=q.size();
             for(int i=0;i<n;i++)
             {
-                TreeNode* node=q.front();
-                if(d>1)
+                TreeNode* node= q.front();
+                if(depth>1)
                 {
-                    if(node->left) q.push(node->left);
-                    if(node->right) q.push(node->right);
+                   if(node->left) q.push(node->left);
+                   if(node->right) q.push(node->right);
                 }
-                else 
+                else
                 {
                     node->left = new TreeNode(val,node->left,NULL);
-                    node->right = new  TreeNode(val,NULL,node->right);
+                    node->right = new TreeNode(val,NULL,node->right);
                 }
                 q.pop();
             }
